696162bef62f679ec6f2fb477ed71085eb5697ba
Akbar-07 <siriyusabbos@gmail.com>
2023-10-09 10:59:47 +0300

    sda
    ds

--- Routes/Orders.js
+++ Routes/Orders.js
@@ -110,13 +110,10 @@ for (let i = 0; i < result2.rows.length; i++) {
  }
 }
 res.json(data);
-
     } catch (error) {
       console.error('Error:', error);
       res.status(500).json({ error: 'Internal Server Error' });
     }
-
-
 })
 
 router.put('/orders/:id', async (req, res) => {
