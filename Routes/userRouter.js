ef54cf0ab5b94354a6f1ac00817ecadbe62b3f26
Akbar-07 <siriyusabbos@gmail.com>
2023-10-09 15:04:36 +0300

    wer
    wer

--- Routes/userRouter.js
+++ Routes/userRouter.js
@@ -4,10 +4,10 @@ const jwt = require('jsonwebtoken')
 const userController = require("../Controller/userController.js");
 const auth = require('../middleware/auth.js');
 
-userRouter.get("/users" ,auth.authenticateToken, userController.getAll);
-userRouter.delete("/users/:id",auth.authenticateToken,userController.deleteUser);
+userRouter.get("/users" ,auth.authenticateToken,auth.isAdmin, userController.getAll);
+userRouter.delete("/users/:id",auth.authenticateToken,auth.isAdmin,userController.deleteUser);
 userRouter.put("/users/:id",auth.authenticateToken,userController.updateUser);
-userRouter.put("/users/:id",auth.authenticateToken,userController.updateUser2);
+userRouter.put("/users/:id",auth.authenticateToken,auth.isAdmin,userController.updateUser2);
 
 userRouter.post("/register",auth.authenticateToken,userController.createUser);
 userRouter.post("/login",userController.loginUser);
