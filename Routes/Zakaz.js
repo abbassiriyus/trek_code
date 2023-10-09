e4a37a3a15dbbe44e7e78ce296159dff5a76bf87
Akbar-07 <siriyusabbos@gmail.com>
2023-10-09 16:39:40 +0300

    sdf
    fsd

--- Routes/Zakaz.js
+++ Routes/Zakaz.js
@@ -75,14 +75,44 @@ router.get('/myzakaz',auth.authenticateToken, async (req, res) => {
      
       const query = 'SELECT * FROM zakaz';
       const query2= 'SELECT * FROM points'
+      const query3= 'SELECT * FROM users'
+      const query4= 'SELECT * FROM orders'
       const result = await pool.query(query);
       const result2 = await pool.query(query2);
+      const result3 = await pool.query(query3);
+      const result4 = await pool.query(query4);
+
 for (let i = 0; i < result.rows.length; i++) {
-  result.rows[i].ponts=[]
+  result.rows[i].ponts=[] 
+  result.rows[i].create=[] 
+  result.rows[i].meneger=[]
 for (let j = 0; j < result2.rows.length; j++) {
 if(result.rows[i].id==result2.rows[j].zakaz_id){
   result.rows[i].ponts.push(result2.rows[j])
-}}}
+}}
+ 
+for (let j = 0; j < result3.rows.length; j++) {
+if(result.rows[i].menegerid==result3.rows[j].id){
+  var a=result3.rows[j]
+  a.password="*******"
+  a.email="*******@gmail.com"
+  a.id="***"
+  result.rows[i].meneger=a
+}
+if(result.rows[i].creator==result3.rows[j].id){
+  var a=result3.rows[j]
+  a.password="*******"
+  a.email="*******@gmail.com"
+  a.id="***"
+  result.rows[i].create=a
+}
+}
+  result.rows[i].oreder=[]
+for (let j = 0; j < result4.rows.length; j++) {
+if(result.rows[i].oredersid==result4.rows[j].id){
+  result.rows[i].oreder=result4.rows[j]
+}}
+}
 var a=result.rows.filter(item=>item.creator==req.user.userId)
 res.json(a);
     } catch (error) {
@@ -94,14 +124,44 @@ router.get('/mezakaz',auth.authenticateToken, async (req, res) => {
     try {
       const query = 'SELECT * FROM zakaz';
       const query2= 'SELECT * FROM points'
+      const query3= 'SELECT * FROM users'
+      const query4= 'SELECT * FROM orders'
       const result = await pool.query(query);
       const result2 = await pool.query(query2);
+      const result3 = await pool.query(query3);
+      const result4 = await pool.query(query4);
+
 for (let i = 0; i < result.rows.length; i++) {
-  result.rows[i].ponts=[]
-for (let j = 0; j < result2.rows.length; j++) {
-if(result.rows[i].id==result2.rows[j].zakaz_id){
+  result.rows[i].ponts=[] 
+  result.rows[i].create=[] 
+  result.rows[i].meneger=[]
+ for (let j = 0; j < result2.rows.length; j++) {
+ if(result.rows[i].id==result2.rows[j].zakaz_id){
   result.rows[i].ponts.push(result2.rows[j])
-}}}
+}}
+ 
+for (let j = 0; j < result3.rows.length; j++) {
+if(result.rows[i].menegerid==result3.rows[j].id){
+  var a=result3.rows[j]
+  a.password="*******"
+  a.email="*******@gmail.com"
+  a.id="***"
+  result.rows[i].meneger=a
+}
+if(result.rows[i].creator==result3.rows[j].id){
+  var a=result3.rows[j]
+  a.password="*******"
+  a.email="*******@gmail.com"
+  a.id="***"
+  result.rows[i].create=a
+}
+}
+  result.rows[i].oreder=[]
+for (let j = 0; j < result4.rows.length; j++) {
+if(result.rows[i].oredersid==result4.rows[j].id){
+  result.rows[i].oreder=result4.rows[j]
+}}
+}
 var a=result.rows.filter(item=>item.menegerid==req.user.userId)
 res.json(a);
     } catch (error) {
