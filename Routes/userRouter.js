e7d30cf45fa8a132f2d8629b6e362e1cc084784f
Akbar-07 <siriyusabbos@gmail.com>
2023-10-10 13:02:25 +0300

    asd
    asd

--- Routes/userRouter.js
+++ Routes/userRouter.js
@@ -7,7 +7,7 @@ const auth = require('../middleware/auth.js');
 userRouter.get("/users" ,auth.authenticateToken,auth.isAdmin, userController.getAll);
 userRouter.delete("/users/:id",auth.authenticateToken,auth.isAdmin,userController.deleteUser);
 userRouter.put("/users/:id",auth.authenticateToken,userController.updateUser);
-userRouter.put("/users/:id",auth.authenticateToken,auth.isAdmin,userController.updateUser2);
+userRouter.put("/admin/:id",auth.authenticateToken,auth.isAdmin,userController.updateUser2);
 
 userRouter.post("/register",auth.authenticateToken,userController.createUser);
 userRouter.post("/login",userController.loginUser);
